d4e6fc54c99b6986c7ccc99a28d7c526b0852e66
yj226 <yj226@cornell.edu>
2023-03-19 19:52:51 -0400

    Implement Comparison.compare_card and Comparison.sort - by Jenny Jin yj226@cornell.edu

--- src/comparison.ml
+++ src/comparison.ml
@@ -1,16 +1,21 @@
+exception Empty
+
+type choice =
+  | Continue of int list
+  | Skip
+
 (** [compare_card x y] compares two cards x and y, returns 1 if card x has a
     higher rank than the card y, returns -1 if x has a smaller rank than y,
     returns 0 if they have the same rank. Note the jokers are the greatest *)
 let compare_card x y =
-  if x = 53 then 1
+  if x = 54 then 1
+  else if y = 54 then -1
+  else if x = 53 then 1
   else if y = 53 then -1
-  else if x = 52 then 1
-  else if y = 52 then -1
   else
     let x_num = x mod 13 in
     let y_num = y mod 13 in
     if x_num < y_num then -1 else if x_num > y_num then 1 else 0
 
-(** [sort cards] sorts the list of cards in ascending order based on rank of the
-    card *)
+(** [sort cards] sorts the list of cards in ascending order based on card rank *)
 let sort (cards : int list) : int list = List.sort compare_card cards
