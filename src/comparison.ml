faaea9f773f57d5dbcda3c32116a36771e94eb59
yj226 <yj226@cornell.edu>
2023-03-19 20:32:53 -0400

    Implement Comparison.single - by Jenny Jin yj226@cornell.edu

--- src/comparison.ml
+++ src/comparison.ml
@@ -19,3 +19,16 @@ let compare_card x y =
 
 (** [sort cards] sorts the list of cards in ascending order based on card rank *)
 let sort (cards : int list) : int list = List.sort compare_card cards
+
+(** [single this other] returns the card to put down in response to what the
+    other player just put down, returns Continue [card] if there is a card in
+    [this] with a greater rank than the opponent's card, returns Skip if there
+    are no cards greater *)
+let rec single (this : int list) (other : int list) : choice =
+  if this = [] then raise Empty
+  else
+    match this with
+    | [] -> Skip
+    | h :: t ->
+        if compare_card h (List.hd other) = 1 then Continue [ h ]
+        else single t other
