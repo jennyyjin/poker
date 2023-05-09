7cc62666a0a4e3595ef7c90297defc60a7fcd92a
Steven Wei Chen <sc2342@cornell.edu>
2023-05-09 16:58:47 -0400

    Implement more test cases -Steven, sc2342 -Implement assertions to check the other type

--- test/main.ml
+++ test/main.ml
@@ -78,6 +78,8 @@ let compare_card_tests =
     compare_card_test "compare_card 0 39 is 0" 0 39 0;
   ]
 
+(**[single_test name card1 card2 expected_outputs] ensures the correctness of
+   the single card that can be put down by the player *)
 let single_test (name : string) (card1 : int list) (card2 : int list)
     (expected_output : choice) : test =
   name >:: fun _ -> assert_equal (single card1 card2) expected_output
@@ -94,30 +96,137 @@ let single_tests =
       "When you have a deck greater than the oppoents, you can continue with \
        that deck"
       [ 4; 5; 6 ] [ 1 ] (Continue [ 4 ]);
+    single_test
+      "When you have a smaller deck than the other put down, you are unable to \
+       continue because the mod 13 value of each is less than the card puyt \
+       down by other"
+      [ 0; 13; 26 ] [ 4 ] Other;
+    single_test
+      "When you have a bigger deck than the card other put down, you can \
+       simply return that earliest deck"
+      [ 14; 12; 13 ] [ 24 ] (Continue [ 12 ]);
   ]
 
+(**[double_test name card1 card2 expected_outputs] ensures the correctness of
+   the double card that can be put down by the player *)
 let double_test (name : string) (card1 : int list) (card2 : int list)
     (expected_output : choice) : test =
   name >:: fun _ -> assert_equal (double card1 card2) expected_output
 
-let double_tests = []
+let double_tests =
+  [
+    double_test
+      "When you dont have enough card, ie. no card, you may simply skip" []
+      [ 0; 13 ] Other;
+    double_test
+      "When you dont have enough card, ie. one card, you may simply skip" [ 10 ]
+      [ 0; 13 ] Other;
+    double_test
+      "When you do have enough card, but not a greater deck than the other \
+       play, you simply skip"
+      [ 2; 14; 1 ] [ 5; 18 ] Other;
+    double_test
+      "When you have a bigger double deck than the other person's card, you \
+       can continue with that card"
+      [ 1; 14; 27 ] [ 0; 13 ]
+      (Continue [ 1; 14 ]);
+    double_test
+      "When you have a bigger double deck than the other person's card and \
+       scramble up, you can continue with that card"
+      [ 14; 28; 1 ] [ 0; 13 ]
+      (Continue [ 14; 1 ]);
+  ]
 
+(**[triple_test name card1 card2 expected_outputs] ensures the correctness of
+   the double card that can be put down by the player *)
 let triple_test (name : string) (card1 : int list) (card2 : int list)
     (expected_output : choice) : test =
   name >:: fun _ -> assert_equal (triple card1 card2) expected_output
 
-let triple_tests = []
+let triple_tests =
+  [
+    triple_test
+      "When you dont have enough card, ie. no card, you may simply skip" []
+      [ 0; 13; 26 ] Other;
+    triple_test
+      "When you dont have enough card, ie. one card, you may simply skip" [ 10 ]
+      [ 0; 13; 26 ] Other;
+    triple_test
+      "When you dont have enough card, ie. two card, you may simply skip"
+      [ 10; 20 ] [ 0; 13; 26 ] Other;
+    triple_test
+      "When you do have enough card, but not a greater deck than the other \
+       play, you simply skip"
+      [ 0; 13; 26 ] [ 1; 14; 27 ] Other;
+    triple_test
+      "When you have a bigger double deck than the other person's card, you \
+       can continue with that card"
+      [ 1; 14; 27 ] [ 0; 13; 26 ]
+      (Continue [ 1; 14; 27 ]);
+    triple_test
+      "When you have a bigger double deck than the other person's card and \
+       scramble up, you can continue with that card"
+      [ 0; 12; 27; 14; 1; 10 ] [ 0; 13; 26 ]
+      (Continue [ 27; 14; 1 ]);
+  ]
 
-let quad_test (name : string) (card1 : int list) (expected_output : choice) :
+(** [quad_test name card expected_ouput] asserts the correctness and quality of
+    the function [quad lst] in comparison*)
+let quad_test (name : string) (card : int list) (expected_output : choice) :
     test =
-  name >:: fun _ -> assert_equal (quad card1) expected_output
+  name >:: fun _ -> assert_equal (quad card) expected_output
 
-let quad_tests = []
+let quad_tests =
+  [
+    quad_test
+      "When you have no card, you can't have a quad combination, so you can \
+       only skip"
+      [] Other;
+    quad_test
+      "When you have one card, you can't have a quad combination, so you can \
+       only skip"
+      [ 0 ] Other;
+    quad_test
+      "When you have two card, you can't have a quad combination, so you can \
+       only skip"
+      [ 10; 20 ] Other;
+    quad_test
+      "When you have three card, you can't have a quad combination, so you can \
+       only skip"
+      [ 0; 13; 26 ] Other;
+    quad_test
+      "You can continue if you posess any forms of quad card in your deck. In \
+       this case [0;13;26;39] is the four 3s in poker"
+      [ 0; 13; 26; 39 ]
+      (Continue [ 0; 13; 26; 39 ]);
+    quad_test
+      "You can continue if you posess any forms of quad card in your deck. In \
+       this case [1;14;27;40] is the four 4s in poker"
+      [ 1; 14; 27; 40 ]
+      (Continue [ 1; 14; 27; 40 ]);
+    quad_test
+      "You can continue if you posess any forms of quad card in your deck. In \
+       this case [1;14;27;40] is the four 4s in poker"
+      [ 1; 27; 28; 7; 2; 14; 4; 40 ]
+      (Continue [ 1; 27; 14; 40 ]);
+    quad_test
+      "You can continue if you posess any forms of quad card in your deck. In \
+       this case [1;14;27;40] is the four 4s in poker"
+      [ 7; 12; 1; 3; 48; 9; 53; 17; 14; 6; 40; 2; 27 ]
+      (Continue [ 1; 14; 40; 27 ]);
+  ]
 
 let comparison_tests =
   [
     "test suite for compare_card"
-    >::: List.flatten [ compare_card_tests; single_tests ];
+    >::: List.flatten
+           [
+             compare_card_tests;
+             single_tests;
+             double_tests;
+             triple_tests;
+             quad_tests;
+           ];
   ]
 
 (**[string_of_int_list lst] is the printer functions that print out the result
